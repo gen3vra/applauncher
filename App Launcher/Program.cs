a773ff894d57265d907bdd3c9e7a1690d51c7757
gen3vra <git@gen.work>
2022-02-28 00:08:44 -0800

    Old source release

---
+++ App Launcher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string directory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        try
+        {
+            string appLaunch = File.ReadAllText(directory + $@"\{Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetExecutingAssembly().Location)}.txt");
+            ProcessStartInfo steamApp = new ProcessStartInfo
+            {
+                FileName = $"\"{appLaunch}\"",
+                Arguments = ""
+            };
+            Process.Start(steamApp);
+        }
+        catch (Exception e)
+        {
+            //Todo: Show some sort of window pop up or something. 
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# App Launcher
+Why would I want this? 
+
+In my case, I needed a program to launch Windows 10 apps like Groove music. This program goes hand in hand with https://github.com/GMR516/steamlauncher
+
+And
+https://gmr516.github.io/customnativetile/
+
+# How do I use this?
+Simply download the release, paste the exact link of the program you want to launch (in my case I created a shortcut of Groove music, and launch that shortcut with this app).
+
+### Folder Layout
+![Folder layout test.](https://i.imgur.com/Mwa4nuZ.png)
+![Shortcut properties](https://i.imgur.com/D0UcIYL.png)
+
+If you need help getting UWP app shortcuts, [this may help](https://web.archive.org/web/20191105063518/https://www.groovypost.com/howto/create-desktop-shortcuts-windows-10-universal-apps/).
